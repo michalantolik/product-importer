e74ba899a5fc2ea0eed4aa20c528eb02e5601350
michalantolik <michal.antolik@gmail.com>
2024-03-05 11:27:41 +0100

    Use csv files from csproj dir (not from output)

--- ProductImporter/ProductImporter/Shared/Configuration.cs
+++ ProductImporter/ProductImporter/Shared/Configuration.cs
@@ -1,10 +1,30 @@
-﻿namespace ProductImporterApp.Shared;
+﻿using System.Reflection;
+
+namespace ProductImporterApp.Shared;
 
 public class Configuration
 {
-    // We will deal with passing in configuration in a better way in a future module
-    // For now hardcoding the values is enough to practice with the concepts from this module
+    public string SourceCsvPath => Path.Combine(ProjectPath, InputFilename);
+    public string TargetCsvPath => Path.Combine(ProjectPath, OutputFilename);
 
-    public string SourceCsvPath => @"c:\src-pluralsight\product-input.csv";
-    public string TargetCsvPath => @"c:\src-pluralsight\product-output.csv";
+    internal string InputFilename => "product-input.csv";
+    internal string OutputFilename => "product-output.csv";
+    internal string ProjectPath
+    {
+        get
+        {
+            // Get the location of the currently executing assembly (which could be in a bin\Debug or bin\Release folder).
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            string projectPath = Path.GetDirectoryName(assemblyLocation);
+
+            // Check if the assembly location contains "\bin", which indicates it's inside a bin folder.
+            // If it is, navigate up until reaching the folder that contains the .csproj file.
+            while (!Directory.GetFiles(projectPath, "*.csproj").Any() && Directory.GetParent(projectPath) != null)
+            {
+                projectPath = Directory.GetParent(projectPath).FullName;
+            }
+
+            return projectPath;
+        }
+    }
 }
