38570f8a12fa73f62f57d7b1edb614ab026ab74c
michalantolik <michal.antolik@gmail.com>
2024-03-05 11:06:14 +0100

    Add usings in Program.cs

--- ProductImporter/ProductImporter/Program.cs
+++ ProductImporter/ProductImporter/Program.cs
@@ -1,3 +1,9 @@
-﻿
+﻿using ProductImporterApp;
+using ProductImporterApp.Model;
+using ProductImporterApp.Shared;
+using ProductImporterApp.Source;
+using ProductImporterApp.Target;
+
 
 Console.WriteLine("Hello ProductImporter !!!");
+
